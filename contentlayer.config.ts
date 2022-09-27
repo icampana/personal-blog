f5f0b8eab0a9189b5c6e1ccb57c08dbb468b9526
Iván Campaña <ivan.campana@gmail.com>
2022-09-27 16:47:36 -0500

    Content layer configuration

---
+++ contentlayer.config.ts
@@ -0,0 +1,60 @@
+import { defineDocumentType, makeSource } from 'contentlayer/source-files'
+import readingTime from 'reading-time';
+
+const Post = defineDocumentType(() => ({
+  name: 'Post',
+  filePathPattern: `**/*.md`,
+  fields: {
+    title: {
+      type: 'string',
+      description: 'The title of the post',
+      required: true,
+    },
+    date: {
+      type: 'date',
+      description: 'The date of the post',
+      required: true,
+    },
+    path: {
+      type: 'string',
+      description: 'The custom slug for the post',
+      required: false,
+    },
+    featuredImage: {
+      type: 'string',
+      description: 'The hero image for the post',
+      required: false,
+    },
+    description: {
+      type: 'string',
+      description: 'Simple text description',
+      required: false,
+    },
+    tags: {
+      type: 'list',
+      of: { type: 'string' },
+      description: 'Associated tags for this post',
+      required: false
+    }
+  },
+  computedFields: {
+    readingTime: { type: 'json', resolve: (doc) => readingTime(doc.body.raw) },
+    url: {
+      type: 'string',
+      resolve: (doc) => {
+
+        if (doc.path) {
+          return `/posts${doc.path}`;
+        }
+
+        return `/posts/${doc._raw.flattenedPath}`;
+      },
+    },
+  },
+}))
+
+export default makeSource({
+  contentDirPath: 'content/posts',
+  documentTypes: [Post],
+  onExtraFieldData: 'ignore'
+})
--- next.config.js
+++ next.config.js
@@ -1,7 +1,25 @@
 /** @type {import('next').NextConfig} */
+const { withContentlayer } = require('next-contentlayer');
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async redirects() {
+    return [
+      // Posts that came from the Gatsby format.
+      {
+        source: '/:slug(\\d{4}\-\\d{2}-\\d{2}-[a-zA-Z\-]{1,})',
+        destination: '/posts/:slug',
+        permanent: true
+      },
+      // Old Posts that came from Blogger / Wordpress
+      {
+        source: '/:year(\\d{4})/:month(\\d{2})/:slug([a-zA-Z\-]{1,})',
+        destination: '/posts/:year/:month/:slug',
+        permanent: true
+      }
+    ]
+  }
 }
 
-module.exports = nextConfig
+module.exports = withContentlayer(nextConfig);
