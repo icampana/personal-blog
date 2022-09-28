e25c62266ed3f5d475698cd79a6e7d7bf3492c12
Iván Campaña <ivan.campana@gmail.com>
2022-09-28 10:40:17 -0500

    Improved the articles display

--- pages/posts/[[...slug]].tsx
+++ pages/posts/[[...slug]].tsx
@@ -70,24 +70,27 @@ const PostLayout = ({ post }: { post: Post }) => {
         <title>{post.title} | { site.title }</title>
       </Head>
       <article className="min-w-min max-w-2xl mx-auto py-8 sm:px-3">
-        <div className='text-left'>
-          <strong className='font-sans font-bold text-3xl mb-6 hover:underline'>
-            <Link href={"/"}>{ site.title }</Link>
-          </strong>
-        </div>
-        <div className="mb-8">
-          <h1 className='font-sans font-bold text-3xl'>{post.title}</h1>
-          <div className='float-right text-gray-400 text-xs'>
-            <em>Tiempo de lectura:</em> {readingTime}
+        <header>
+          <div className='text-left'>
+            <strong className='font-sans font-bold text-3xl mb-6'>
+              <Link href={"/"}><a>{ site.title }</a></Link>
+            </strong>
           </div>
-          <DateComponent postDate={post.date} />
-        </div>
+          <div className="mb-8 mt-3">
+            <h1 className='text-center font-sans font-bold text-3xl text-orange-900'>{post.title}</h1>
+            <div className='float-right text-gray-400 text-xs'>
+              <em>Tiempo de lectura:</em> {readingTime}
+            </div>
+            <DateComponent postDate={post.date} />
+          </div>
+        </header>
 
         <div style={{ position: 'relative', width: '100%', height: '250px', marginBottom: '10px' }}>
           {imagePath && <Image src={imagePath} alt={post.title} layout="fill" objectFit="cover"  placeholder="blur" blurDataURL='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mM0SeuuBwADoQGm9h5VIAAAAABJRU5ErkJggg==' />}
         </div>
 
-        <div className='leading-7' dangerouslySetInnerHTML={{ __html: post.body.html }} />
+        <div className='leading-7 px-2' dangerouslySetInnerHTML={{ __html: post.body.html }} />
+
          {/* Back to top button */}
         {showReturn && <button onClick={backToTop} type="button" data-mdb-ripple="true" data-mdb-ripple-color="light" className="fixed inline-block p-3 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out bottom-5 right-5" id="btn-back-to-top">
           <svg aria-hidden="true" focusable="false" data-prefix="fas" className="w-4 h-4" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M34.9 289.5l-22.2-22.2c-9.4-9.4-9.4-24.6 0-33.9L207 39c9.4-9.4 24.6-9.4 33.9 0l194.3 194.3c9.4 9.4 9.4 24.6 0 33.9L413 289.4c-9.5 9.5-25 9.3-34.3-.4L264 168.6V456c0 13.3-10.7 24-24 24h-32c-13.3 0-24-10.7-24-24V168.6L69.2 289.1c-9.3 9.8-24.8 10-34.3.4z"></path></svg>
