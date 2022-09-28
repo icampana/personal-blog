fbb8e37c239b3ad50027454eb5ccbb9926f97512
Iván Campaña <ivan.campana@gmail.com>
2022-09-28 10:26:10 -0500

    Improved the date color and added translation

--- components/blocks/Date.tsx
+++ components/blocks/Date.tsx
@@ -1,13 +1,13 @@
 import { format, parseISO } from "date-fns";
-
+import { es } from 'date-fns/locale'
 interface DateComponentProps {
     postDate: string
 }
 
 const DateComponent = ({ postDate } : DateComponentProps) => {
     return (
-        <time dateTime={postDate} className="text-xs text-gray-600 mb-1">
-            {format(parseISO(postDate), "LLLL d, yyyy")}
+        <time dateTime={postDate} className="capitalize text-xs text-gray-400 mb-1">
+            {format(parseISO(postDate), "MMMM d, yyyy", {locale: es})}
         </time>
     );
 }
