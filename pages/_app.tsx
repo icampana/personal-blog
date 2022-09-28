846e5ff98531ac3c0538ef8d9105b1a92cb7177f
Iván Campaña <ivan.campana@gmail.com>
2022-09-28 12:11:15 -0500

    Added Basic SEO data

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,31 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { DefaultSeo } from 'next-seo';
+import meta from 'metadata.json';
 
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 config.autoAddCss = false
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const { site } = meta;
+
+  return (<>
+    <DefaultSeo
+      openGraph={{
+        type: 'website',
+        locale: 'es',
+        url: site.siteUrl,
+        site_name: site.title,
+      }}
+      twitter={{
+        handle: `@${site.social.twitter}`,
+        site: '@site',
+        cardType: 'summary_large_image',
+      }}
+    />
+    <Component {...pageProps} />
+  </>);
 }
 
 export default MyApp
