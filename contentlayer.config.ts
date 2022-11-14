727a5d8d31f4253e7be68663969a565c5c8c98f8
Iván Campaña <ivan.campana@gmail.com>
2022-11-14 01:17:33 -0500

    Added custom youtube parser for videos.

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,10 +1,17 @@
 import { defineDocumentType, makeSource } from 'contentlayer/source-files'
 import readingTime from 'reading-time';
 import remarkEmoji from 'remark-emoji';
-import remarkHtml from 'remark-html';
+import remarkFrontmatter from 'remark-frontmatter'
+import rehypeStringify from 'rehype-stringify'
+import remark2rehype from 'remark-rehype'
+import remarkParse from 'remark-parse'
+import remarkGfm from 'remark-gfm'
+import remarkBreaks from 'remark-breaks'
+import { visit } from 'unist-util-visit';
 
-import remarkEmbedder from '@remark-embedder/core'
-import oembedTransformer from '@remark-embedder/transformer-oembed'
+// import remarkEmbedder from '@remark-embedder/core'
+// import oembedTransformer from '@remark-embedder/transformer-oembed'
+// import type {Config} from '@remark-embedder/transformer-oembed'
 
 const Post = defineDocumentType(() => ({
   name: 'Post',
@@ -58,11 +65,54 @@ const Post = defineDocumentType(() => ({
   },
 }))
 
+function videoPlugin(options?: any | undefined): any | void {
+  const youtubeSearch = RegExp(/(?:https?:\/\/)?(?:www\.)?youtu(?:\.be\/|be.com\/\S*(?:watch|embed)(?:(?:(?=\/[-a-zA-Z0-9_]{11,}(?!\S))\/)|(?:\S*v=|v\/)))([-a-zA-Z0-9_]{11,})/);
+  function transformer (tree: any) {
+      visit(tree, 'paragraph', function (paragraphNode) {
+        const {
+          children
+        } = paragraphNode;
+        const node = children[0]
+
+        if (node.type === 'link') {
+          const matches = youtubeSearch.exec(node.url);
+          if (matches && node.children && node.children[0].type === 'text' && node.children[0].value === node.url) {
+            const videoId = matches[1];
+            node.type = 'html';
+            node.value = `<iframe width="320" height="240" src="https://www.youtube.com/embed/${videoId}?&autoplay=1" frameborder="0" allowfullscreen style="margin: 0 auto;"></iframe>`;
+            delete node.children;
+            delete node.url;
+          }
+
+        }
+      });
+  }
+
+  return transformer;
+}
+
 export default makeSource({
   contentDirPath: 'content/posts',
   documentTypes: [Post],
   onExtraFieldData: 'ignore',
-  markdown: { remarkPlugins: [ remarkEmbedder, remarkEmoji, remarkHtml] }
-  // , {transformers: [oembedTransformer.default]}]
+  markdown: (builder) => {
+    builder
+      .use(remarkFrontmatter)
+      .use(remarkParse)
+      .use(videoPlugin)
+      // .use(remarkEmbedder, {
+      //   transformers: [
+      //     oembedTransformer
+      //   ]
+      //  })
+      .use(remarkGfm)
+      .use(remarkBreaks)
+      .use(remarkEmoji)
+      .use(remark2rehype, {allowDangerousHtml: true})
+      .use(rehypeStringify, {allowDangerousHtml: true})
+      // .use(() => (tree: any) => {
+      //   console.dir(tree.children.slice(-1)[0].children)
+      // })
+  }
 })
 //
