3aee9b66cd88e65798a77fb988f0ccd8ea90f4c5
Iván Campaña <ivan.campana@gmail.com>
2022-11-19 11:30:40 -0500

    Added meta verification tag for google

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,6 +16,7 @@ class MyDocument extends Document {
             <link rel="preconnect" href="https://fonts.googleapis.com" />
             <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
             <link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet" />
+            <meta name="google-site-verification" content="b5jnO_8lDhyKJ6m0nMqj0ZD0DMuvGkgBA0JSwUTcTf8" />
         </Head>
         <body>
           <Main />
