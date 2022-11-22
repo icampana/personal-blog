f22977af6ae4aea224c4c315ba1963925af452c5
Iván Campaña <ivan.campana@gmail.com>
2022-11-21 23:35:42 -0500

    Removed viewport definition

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,16 +5,12 @@ import meta from 'metadata.json';
 
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
-import Head from 'next/head';
 config.autoAddCss = false
 
 function MyApp({ Component, pageProps }: AppProps) {
   const { site } = meta;
 
   return (<>
-    <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    </Head>
     <DefaultSeo
       openGraph={{
         type: 'website',
