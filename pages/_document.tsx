b614e53d386fce79e0803cc72cb0f238f027ea5d
Iván Campaña <ivan.campana@gmail.com>
2022-11-21 23:20:15 -0500

    Fixed vireport information

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,7 +14,6 @@ class MyDocument extends Document {
       <Html lang='es'>
         <Head>
           <meta charSet="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
           <link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet" />
