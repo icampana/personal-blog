f9cb92912832d3ef7fa6e18931c3a27952cdd268
Iván Campaña <ivan.campana@gmail.com>
2025-08-15 22:53:18 +0100

    Update tailwind.config.js to include additional content paths for improved styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./content/**/*.{md,mdx}",
+    "./lib/**/*.{js,ts}",
+    "./tina/**/*.{ts,js}",
   ],
   theme: {
     fontFamily: {
