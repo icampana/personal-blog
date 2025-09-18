8530e680b599b2ae8380e484371c3f72334db0db
Iván Campaña <ivan.campana@gmail.com>
2025-09-18 22:33:14 +0100

    Refactor PostCSS and Tailwind configuration files for improved readability and consistency

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./content/**/*.{md,mdx}",
-    "./lib/**/*.{js,ts}",
-    "./tina/**/*.{ts,js}",
-  ],
-  theme: {
-    fontFamily: {
-      sans: ['Ubuntu', 'sans-serif'],
-      serif: ['Dancing Script', 'serif'],
-    },
-    extend: {},
-  },
-  plugins: [],
-}
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+		"./content/**/*.{md,mdx}",
+		"./lib/**/*.{js,ts}",
+		"./tina/**/*.{ts,js}",
+	],
+	theme: {
+		fontFamily: {
+			sans: ["Ubuntu", "sans-serif"],
+			serif: ["Dancing Script", "serif"],
+		},
+		extend: {},
+	},
+	plugins: [],
+};
