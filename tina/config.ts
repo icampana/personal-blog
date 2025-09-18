816a3dd42450c9a7a9b027f74f63e6526faffee7
Iván Campaña <ivan.campana@gmail.com>
2025-09-18 22:33:42 +0100

    Refactor TinaCMS configuration and templates for improved readability and consistency

--- tina/config.ts
+++ tina/config.ts
@@ -1,96 +1,98 @@
 import { defineConfig } from "tinacms";
-import { postsFields, pagesFields, projectFields } from "./templates";
+import { pagesFields, postsFields, projectFields } from "./templates";
 
 // Your hosting provider likely exposes this as an environment variable
 const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 
 export default defineConfig({
-  branch,
-  clientId: "da403933-32db-4dcf-b799-ebc141c1fd51", // Get this from tina.io
-  token: "d34d778eb69b883ddc7b48c0757e4bfa7999ec25", // Get this from tina.io
-  client: { skip: true },
-  build: {
-    outputFolder: "admin",
-    publicFolder: "public",
-  },
-  media: {
-    tina: {
-      mediaRoot: "",
-      publicFolder: "public",
-    },
-  },
-  schema: {
-    collections: [
-      {
-        format: "md",
-        label: "Posts",
-        name: "posts",
-        path: "content/posts",
-        match: {
-          include: "**/*",
-        },
-        ui: {
-          filename: {
-            // Example of using a custom slugify function
-            slugify: (values) => {
-              const articleDate = values?.date ? new Date(values?.date) : new Date();
+	branch,
+	clientId: "da403933-32db-4dcf-b799-ebc141c1fd51", // Get this from tina.io
+	token: "d34d778eb69b883ddc7b48c0757e4bfa7999ec25", // Get this from tina.io
+	client: { skip: true },
+	build: {
+		outputFolder: "admin",
+		publicFolder: "public",
+	},
+	media: {
+		tina: {
+			mediaRoot: "",
+			publicFolder: "public",
+		},
+	},
+	schema: {
+		collections: [
+			{
+				format: "md",
+				label: "Posts",
+				name: "posts",
+				path: "content/posts",
+				match: {
+					include: "**/*",
+				},
+				ui: {
+					filename: {
+						// Example of using a custom slugify function
+						slugify: (values) => {
+							const articleDate = values?.date
+								? new Date(values?.date)
+								: new Date();
 
-              // Values is an object containing all the values of the form. In this case it is {title?: string, topic?: string}
-              return `${articleDate.getFullYear()}-${articleDate.getMonth() + 1}-${values?.title
-                ?.toLowerCase()
-                .replace(/ /g, '-')}`;
-            },
-          },
-        },
-        fields: [
-          {
-            type: "rich-text",
-            name: "body",
-            label: "Body of Document",
-            description: "This is the markdown body",
-            isBody: true,
-          },
-          ...postsFields(),
-        ],
-      },
-      {
-        format: "md",
-        label: "Pages",
-        name: "pages",
-        path: "content/pages",
-        match: {
-          include: "**/*",
-        },
-        fields: [
-          {
-            type: "rich-text",
-            name: "body",
-            label: "Body of Document",
-            description: "This is the markdown body",
-            isBody: true,
-          },
-          ...pagesFields(),
-        ],
-      },
-      {
-        format: "md",
-        label: "Projects",
-        name: "projects",
-        path: "content/projects",
-        match: {
-          include: "**/*",
-        },
-        fields: [
-          {
-            type: "rich-text",
-            name: "body",
-            label: "Body of Document",
-            description: "This is the markdown body",
-            isBody: true,
-          },
-          ...projectFields(),
-        ],
-      },
-    ],
-  },
+							// Values is an object containing all the values of the form. In this case it is {title?: string, topic?: string}
+							return `${articleDate.getFullYear()}-${articleDate.getMonth() + 1}-${values?.title
+								?.toLowerCase()
+								.replace(/ /g, "-")}`;
+						},
+					},
+				},
+				fields: [
+					{
+						type: "rich-text",
+						name: "body",
+						label: "Body of Document",
+						description: "This is the markdown body",
+						isBody: true,
+					},
+					...postsFields(),
+				],
+			},
+			{
+				format: "md",
+				label: "Pages",
+				name: "pages",
+				path: "content/pages",
+				match: {
+					include: "**/*",
+				},
+				fields: [
+					{
+						type: "rich-text",
+						name: "body",
+						label: "Body of Document",
+						description: "This is the markdown body",
+						isBody: true,
+					},
+					...pagesFields(),
+				],
+			},
+			{
+				format: "md",
+				label: "Projects",
+				name: "projects",
+				path: "content/projects",
+				match: {
+					include: "**/*",
+				},
+				fields: [
+					{
+						type: "rich-text",
+						name: "body",
+						label: "Body of Document",
+						description: "This is the markdown body",
+						isBody: true,
+					},
+					...projectFields(),
+				],
+			},
+		],
+	},
 });
--- tina/templates.ts
+++ tina/templates.ts
@@ -1,98 +1,98 @@
 import type { TinaField } from "tinacms";
 export function pagesFields() {
-  return [
-    {
-      type: "string",
-      name: "title",
-      label: "Page Title",
-      required: true,
-    },
-    {
-      type: "datetime",
-      name: "date",
-      label: "Publication Date",
-      required: true,
-    },
-    {
-      type: "string",
-      name: "description",
-      label: "Short Description",
-      ui: {
-        component: "textarea",
-      },
-    },
-  ] as TinaField[];
+	return [
+		{
+			type: "string",
+			name: "title",
+			label: "Page Title",
+			required: true,
+		},
+		{
+			type: "datetime",
+			name: "date",
+			label: "Publication Date",
+			required: true,
+		},
+		{
+			type: "string",
+			name: "description",
+			label: "Short Description",
+			ui: {
+				component: "textarea",
+			},
+		},
+	] as TinaField[];
 }
 export function postsFields() {
-  return [
-    {
-      type: "string",
-      name: "title",
-      label: "Article Title",
-      required: true,
-      isTitle: true,
-    },
-    {
-      type: "datetime",
-      name: "date",
-      label: "Publication Date",
-      required: true,
-    },
-    {
-      type: "image",
-      name: "featuredImage",
-      label: "Featured Image",
-    },
-    {
-      type: "string",
-      name: "description",
-      label: "Short Description",
-      ui: {
-        component: "textarea",
-      },
-    },
-    {
-      type: "string",
-      name: "tags",
-      label: "Tags",
-      list: true,
-    },
-  ] as TinaField[];
+	return [
+		{
+			type: "string",
+			name: "title",
+			label: "Article Title",
+			required: true,
+			isTitle: true,
+		},
+		{
+			type: "datetime",
+			name: "date",
+			label: "Publication Date",
+			required: true,
+		},
+		{
+			type: "image",
+			name: "featuredImage",
+			label: "Featured Image",
+		},
+		{
+			type: "string",
+			name: "description",
+			label: "Short Description",
+			ui: {
+				component: "textarea",
+			},
+		},
+		{
+			type: "string",
+			name: "tags",
+			label: "Tags",
+			list: true,
+		},
+	] as TinaField[];
 }
 
 export function projectFields() {
-  return [
-    {
-      type: "string",
-      name: "title",
-      label: "Project Title",
-      required: true,
-    },
-    {
-      type: "datetime",
-      name: "date",
-      label: "Publication Date",
-      required: true,
-    },
-    {
-      type: "string",
-      name: "description",
-      label: "Short Description",
-      ui: {
-        component: "textarea",
-      },
-    },
-    {
-      type: "image",
-      name: "galleryImage",
-      label: "Project Image",
-      list: true,
-    },
-    {
-      type: "string",
-      name: "techStack",
-      label: "Technologies",
-      list: true,
-    },
-  ] as TinaField[];
+	return [
+		{
+			type: "string",
+			name: "title",
+			label: "Project Title",
+			required: true,
+		},
+		{
+			type: "datetime",
+			name: "date",
+			label: "Publication Date",
+			required: true,
+		},
+		{
+			type: "string",
+			name: "description",
+			label: "Short Description",
+			ui: {
+				component: "textarea",
+			},
+		},
+		{
+			type: "image",
+			name: "galleryImage",
+			label: "Project Image",
+			list: true,
+		},
+		{
+			type: "string",
+			name: "techStack",
+			label: "Technologies",
+			list: true,
+		},
+	] as TinaField[];
 }
